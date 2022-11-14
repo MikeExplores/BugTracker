d0c2c83f50cb4052ca2c48b503f3111b61fce2d3
mrtsolutions-git <74580826+mrtsolutions-git@users.noreply.github.com>
2022-11-13 19:18:47 -0600

    db initialization

--- db_scripts.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE bug_tracker;
-
-CREATE TABLE `bug_tracker`.`bugs` ( `bug_id` INT NOT NULL AUTO_INCREMENT, `product_name` VARCHAR(255) NOT NULL , `version` VARCHAR(255) NOT NULL , `hardware_type` VARCHAR(255) NOT NULL , `operating_system` VARCHAR(255) NOT NULL , `occurrence_frequency` VARCHAR(255) NOT NULL , `proposed_solution` VARCHAR(255) NOT NULL , PRIMARY KEY (`bug_id`)); 
